165f87e7e2064a04ff00d2df8a66973e7c6d981f
salmadev2 <salmadev231@gmail.com>
2024-12-22 20:52:48 +0100

    feat: Add study-area entity

--- src/study-area/entities/study-area.entity.ts
+++ src/study-area/entities/study-area.entity.ts
@@ -1,1 +1,25 @@
-export class StudyArea {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class StudyArea {
+
+  @Prop({ unique: true, required: true })
+  id: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  presence: boolean;
+
+  @Prop({ required: true })
+  num_cats: number;
+
+  @Prop({ required: true })
+  region: string;
+
+  @Prop()
+  link: string;
+}
+
+export const StudyAreaSchema = SchemaFactory.createForClass(StudyArea);
